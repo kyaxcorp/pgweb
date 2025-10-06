48764640da4216aa780d6bf35fbfc1b78760c836
Octavian T. <octavian@tabuci.org>
2025-10-06 22:29:19 +0300

    docker build

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPO="mrdiffer/pgweb"
+TAG_NAME="$REPO:v1.0.0"
+LATEST="$REPO:latest"
+
+docker buildx create --use --name mounted-build-kit --node mounted-build-kit --bootstrap
+if ! docker buildx build \
+     --builder mounted-build-kit \
+     --push \
+     --platform linux/amd64,linux/arm64 \
+     --tag $TAG_NAME --tag $LATEST -f Dockerfile  .; then
+  echo "build error, terminating script"
+  exit 1
+fi
+docker buildx prune -a -f --builder mounted-build-kit
+
+echo "build completed successfully"
